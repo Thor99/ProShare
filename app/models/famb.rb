20f8583207d066b396c60900c951bd30b0f46d7b
Thor <thor.fc21@gmail.com>
2015-04-26 12:40:00 -0300

    Fixed an error in Famb model

--- app/models/famb.rb
+++ app/models/famb.rb
@@ -1,5 +1,5 @@
 class Famb < ActiveRecord::Base
   belongs_to :project, counter_cache: true
   belongs_to :user
-  validates_uniqueness_of :project_id, :user_id
+  validates_uniqueness_of :project_id, scope: :user_id
 end
--- db/migrate/20150423033008_add_fambs_count_to_projects.rb
+++ db/migrate/20150423033008_add_fambs_count_to_projects.rb
@@ -1,5 +1,5 @@
 class AddFambsCountToProjects < ActiveRecord::Migration
   def change
-    add_column :projects, :fambs_count, :integer
+    add_column :projects, :fambs_count, :integer, :default => 0
   end
 end
